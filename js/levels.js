55bf89ac90733706bdeb4244b95519165b17a4bf
martinskasko <martinskasko@gmail.com>
2024-12-25 15:08:22 +0100

    added favicon, button Hrať

--- js/levels.js
+++ js/levels.js
@@ -5,6 +5,10 @@ const fetchData = async () => {
         const response = await fetch('../assets/levels.json');
         levels = await response.json();
         console.log('Levels fetched:', levels);
+
+        initializeLocalStorage(levels);
+
+
         loadLevels(levels);
     } catch (error) {
         console.error('Error fetching levels:', error);
@@ -12,6 +16,26 @@ const fetchData = async () => {
 };
 
 window.addEventListener('DOMContentLoaded', fetchData);
+
+const initializeLocalStorage = (levels) => {
+    if(!localStorage.getItem('levels')){
+        const levelsData = {};
+        levels.forEach(level => {
+           levelsData[level.id] = false;
+        });
+        localStorage.setItem('levels', JSON.stringify(levelsData));
+    }
+};
+
+const getLevelIdToPlay = () => {
+    const levelsData = JSON.parse(localStorage.getItem('levels'));
+    for(let levelId in levelsData){
+        if(levelsData[levelId] === false){
+            return levelId;
+        }
+    }
+    return Math.floor(Math.random() * 5);
+}
 
 const getBadgeColor = (difficulty) => {
     switch (difficulty.toLowerCase()) {
@@ -67,5 +91,9 @@ const loadLevels = (levels) => {
     }
 };
 
+function play(){
+    const levelId = getLevelIdToPlay();
 
-
+    const levelQuery = `game.html?levelId=${levelId}`;
+    window.location.replace(levelQuery);
+}
